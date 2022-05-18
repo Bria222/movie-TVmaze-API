3256b712067c87c660685c4bc48faa0aa0e6667d
Bria222 <briantechologies.co.ke@gmail.com>
2022-05-18 08:28:14 +0300

    fetch movies from an api

---
+++ script.js
@@ -0,0 +1,46 @@
+function searchShow(query) {
+  const url = `https://api.tvmaze.com/search/shows?q=${query}`;
+  fetch(url)
+    .then(response => response.json())
+    .then((jsonData) => {
+      let shows = jsonData.map(element => element.show);
+      renderShows(shows);
+      document.getElementById("errorMessage").innerHTML = "";
+    })
+    .catch((error) => {
+      document.getElementById("errorMessage").innerHTML = error;
+    })
+}
+
+function renderShows(shows) {
+  const list = document.getElementById("resultsList");
+  list.innerHTML = "";
+  shows.forEach(show => {
+    const element = document.createElement("li");
+    const img = document.createElement("img");
+    img.classList.add('images');
+    const text = document.createElement("span");
+    
+    img.src = show.image.original;
+    text.innerText = show.name;
+    
+    element.appendChild(text);
+    element.appendChild(img);
+    
+    list.appendChild(element);
+
+  });
+}
+
+let searchTimeoutToken = 0;
+
+window.onload = () => {
+  const searchFieldElement = document.getElementById("searchField")
+  searchFieldElement.onkeyup = (event) => {
+    clearTimeout(searchTimeoutToken);
+    searchTimeoutToken = setTimeout(() => {
+      searchShow(searchFieldElement.value);
+    }, 250);
+
+  };
+}
